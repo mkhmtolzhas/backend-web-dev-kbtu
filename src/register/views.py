3dc0a3b7f914bd5e8e5d1aac99eaac4b893c8e38
mkhmtolzhas <mkhmtcore@gmail.com>
2025-04-15 19:04:41 +0500

    feat: added auth

---
+++ src/register/views.py
@@ -0,0 +1,8 @@
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
