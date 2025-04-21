53ff17fdcf962adc127c145353b14bca1a03d6b0
mkhmtolzhas <mkhmtcore@gmail.com>
2025-04-21 19:45:37 +0500

    refactor: just to know port

--- src/src/config.py
+++ src/src/config.py
@@ -11,3 +11,5 @@ S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
 S3_SECRET_KEY = os.getenv('S3_SECRET_KEY')
 S3_REGION = os.getenv('S3_REGION')
+
+print(os.getenv('PORT'))
